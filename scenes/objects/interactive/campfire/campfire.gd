734f38857acfcd9f7cca575efc663ce18f8b3daa
kodiitulip <kodii.tulip@gmail.com>
2025-09-20 16:53:55 -0300

    feat: campfire interact with inventory manager
    + using fuel fromthe inventory

--- scenes/objects/interactive/campfire/campfire.gd
+++ scenes/objects/interactive/campfire/campfire.gd
@@ -2,30 +2,35 @@ class_name CampfireArea
 extends Area2D
 
 signal campfire_heated(heat: float)
-signal campfire_feed(campfire: CampfireArea)
 
 var player: PlayerCharacterBody2D
 var fuel: float = 100
 
 @onready var fuel_bar: ProgressBar = $FuelBar
+@onready var fire_particles: GPUParticles2D = %FireParticles
+@onready var mouse_indicator: TextureRect = $MouseIndicator
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
+	input_event.connect(_on_input_event)
+	campfire_heated.connect(InventoryManager.on_campfire_heating)
 
 
 func _process(delta: float) -> void:
+	fire_particles.emitting = fuel > 0
 	fuel = clampf(fuel - delta * 2.5, 0, 100)
 	fuel_bar.value = fuel
-	if fuel > 0:
-		campfire_heated.emit(delta * 1.5)
+	if player and fuel > 0:
+		campfire_heated.emit(delta * 2.5)
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is not PlayerCharacterBody2D:
 		return
 	player = body
-	campfire_heated.connect(player.on_campfire_heating)
+	if fuel < 100:
+		mouse_indicator.show()
 
 
 func _on_body_exited(body: Node2D) -> void:
@@ -33,12 +38,13 @@ func _on_body_exited(body: Node2D) -> void:
 		return
 	if body != player:
 		return
-	campfire_heated.disconnect(player.on_campfire_heating)
-	
 	player = null
+	mouse_indicator.hide()
 
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed(&"mouse_click_left") and player:
-		pass
-	pass
+		if InventoryManager.fuel <= 0:
+			return
+		InventoryManager.fuel = maxi(InventoryManager.fuel - 1, 0)
+		fuel += 30
